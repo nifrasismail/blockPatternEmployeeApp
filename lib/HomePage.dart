70a615be343886e18e73d6892f1bb594cb167e6c
nifrasismail <nifras321>
2020-02-20 16:53:42 +0530

    rtl support added

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -32,55 +32,58 @@ class _HomePageState extends State<HomePage> {
             return ListView.builder(
               itemCount: snapshot.data.length,
               itemBuilder: (context, index) {
-                return Card(
-                  elevation: 5.0,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.all(20.0),
-                        child: Text(
-                          "${snapshot.data[index].id}.",
-                          style: TextStyle(fontSize: 20.0),
+                return Directionality(
+                  textDirection: TextDirection.ltr,
+                  child: Card(
+                    elevation: 5.0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(20.0),
+                          child: Text(
+                            "${snapshot.data[index].id}.",
+                            style: TextStyle(fontSize: 20.0),
+                          ),
                         ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(20.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              "${snapshot.data[index].name}.",
-                              style: TextStyle(fontSize: 18.0),
-                            ),
-                            Text(
-                              "Rs.${snapshot.data[index].salary}.",
-                              style: TextStyle(fontSize: 16.0),
-                            ),
-                          ],
+                        Container(
+                          padding: EdgeInsets.all(20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                "${snapshot.data[index].name}.",
+                                style: TextStyle(fontSize: 18.0),
+                              ),
+                              Text(
+                                "Rs.${snapshot.data[index].salary}.",
+                                style: TextStyle(fontSize: 16.0),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      Container(
-                        child: IconButton(
-                          icon: Icon(Icons.thumb_up),
-                          color: Colors.green,
-                          onPressed: () {
-                            _employeeBloc.employeeSalaryIncrement
-                                .add(snapshot.data[index]);
-                          },
+                        Container(
+                          child: IconButton(
+                            icon: Icon(Icons.thumb_up),
+                            color: Colors.green,
+                            onPressed: () {
+                              _employeeBloc.employeeSalaryIncrement
+                                  .add(snapshot.data[index]);
+                            },
+                          ),
                         ),
-                      ),
-                      Container(
-                        child: IconButton(
-                          icon: Icon(Icons.thumb_down),
-                          color: Colors.red,
-                          onPressed: () {
-                            _employeeBloc.employeeSalaryDecrement
-                                .add(snapshot.data[index]);
-                          },
+                        Container(
+                          child: IconButton(
+                            icon: Icon(Icons.thumb_down),
+                            color: Colors.red,
+                            onPressed: () {
+                              _employeeBloc.employeeSalaryDecrement
+                                  .add(snapshot.data[index]);
+                            },
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 );
               },
